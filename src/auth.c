6a30ca14bac53ecfecde29062c7ae3be048627da
helouazizi <ouazizi2code@gmail.com>
2025-06-04 15:13:50 +0100

    refactore the login menu

--- src/auth.c
+++ src/auth.c
@@ -1,36 +1,37 @@
-// #include <termios.h>
+#include <termios.h>
 #include "header.h"
 
 
 void loginMenu(char a[50], char pass[50])
  {
-//     struct termios oflags, nflags;
+    struct termios oflags, nflags;
 
-//     system("clear");
-//     printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
-//     scanf("%s", a);
+    system("clear");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
+    scanf("%s", a);
 
-//     // disabling echo
-//     tcgetattr(fileno(stdin), &oflags);
-//     nflags = oflags;
-//     nflags.c_lflag &= ~ECHO;
-//     nflags.c_lflag |= ECHONL;
+    // disabling echo
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
 
-//     if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
-//     {
-//         perror("tcsetattr");
-//         return exit(1);
-//     }
-//     printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
-//     scanf("%s", pass);
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
+    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+    scanf("%s", pass);
 
-//     // restore terminal
-//     if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
-//     {
-//         perror("tcsetattr");
-//         return exit(1);
-//     }
+    // restore terminal
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
 }; 
+
 
 // const char *getPassword(struct User u)
 // {
