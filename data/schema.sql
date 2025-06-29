3ced82961c89fbb6b5875482ecf8d27658e5be95
helouazizi <ouazizi2code@gmail.com>
2025-06-29 10:36:47 +0100

    fix: update schema.sql to correct table creation syntax and improve formatting

--- data/schema.sql
+++ data/schema.sql
@@ -1,13 +1,20 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,   -- internal unique id
-    username TEXT UNIQUE NOT NULL,           -- unique username
-    password TEXT NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, -- internal unique id
+        username TEXT UNIQUE NOT NULL, -- unique username
+        password TEXT NOT NULL
+    );
 
-CREATE TABLE IF NOT EXISTS records (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,   -- internal unique id for accounts
-    accountNbr INTEGER UNIQUE NOT NULL,     -- account number chosen by user
-    ownerUsername TEXT NOT NULL,
-    balance REAL DEFAULT 0,
-    FOREIGN KEY(ownerUsername) REFERENCES users(username)
-);
+CREATE TABLE
+    IF NOT EXISTS records (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        country TEXT NOT NULL,
+        phone TEXT NOT NULL,
+        acount_type TEXT NOT NULL,
+        accountNbr INTEGER UNIQUE NOT NULL,
+        amount REAL DEFAULT 0,
+        diposit DATE,
+        withdraw DATE,
+        FOREIGN KEY (user_id) REFERENCES users (id)
+    );
