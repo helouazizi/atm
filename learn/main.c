3cc1f4a46fcdaf0cb6e4413d8e1fc5a8b96007c2
helouazizi <ouazizi2code@gmail.com>
2025-08-05 10:35:44 +0100

    start a procces learn

---
+++ learn/main.c
@@ -0,0 +1,16 @@
+// learn/main.c
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+    printf("%s\n","hello from low level");
+    // lets store he file descriptors 
+    int fd[2];
+    // pip_t pid;
+    char buffer[50];
+
+    
+}
